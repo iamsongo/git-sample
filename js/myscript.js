68f81b1cc64b92aebde94b3758e73b95148d5860
Unknown <iamsongo@gmail.com>
2017-06-14 17:40:37 +0700

    Merge Edit

--- js/myscript.js
+++ js/myscript.js
@@ -6,12 +6,10 @@ function develop2() {
 	return 0;
 }
 
-<<<<<<< HEAD
 function addfriend(id) {
 	return 0;
 }
-=======
+
 function addfriend(phone) {
-	#addfriend_id;
+	//addfriend_id;
 }
->>>>>>> add-friend-by-phone
